be111d659e6e7b3fefecb68333ba0157fe6bef4a
joseantoniols1212 <joseantonioluquesalguero1212@gmail.com>
2025-08-29 14:09:53 +0200

    feat: modal component

---
+++ url-shortener-frontend/src/app/shared/components/modal/modal.component.ts
@@ -0,0 +1,17 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  imports: [],
+  templateUrl: './modal.component.html',
+})
+export class ModalComponent {
+
+  id = input.required<string>();
+  textModal = input.required<string>();
+
+  openModal() {
+    const dialog = <HTMLDialogElement>document.getElementById(this.id());
+    dialog?.showModal();
+  }
+}
