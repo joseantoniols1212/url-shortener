d8b4fea0f62250d70b077d1d2d2561c20f5584b2
joseantoniols1212 <joseantonioluquesalguero1212@gmail.com>
2025-09-16 14:24:32 +0200

    feat: extremely basic register and login api endpoints

--- url-shortener-backend/src/auth/login/app.py
+++ url-shortener-backend/src/auth/login/app.py
@@ -1,13 +1,71 @@
 import json
 import boto3
+from boto3.dynamodb.conditions import Key
 import os
 import jwt
+import hashlib
+
+import bcrypt
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(os.environ["USER_TABLE_NAME"])
+hash_object = hashlib.sha256()
+
+JWT_SECRET = os.environ["JWT_SECRET"]
+
+def response(status, body_dict):
+    return {
+        "statusCode": status,
+        "headers": {
+            "Content-Type": "application/json",
+        },
+        "body": json.dumps(body_dict)
+    }
+
 
 def handler(event, _context):
 
-    token = jwt.encode({'key': 'value'}, 'secret', algorithm='HS256')
+    request_body = event['body']
 
-    body = json.dumps({ "token": token })
+    try:
+        parsed_body = json.loads(request_body)
+        email = parsed_body['email']
+        password = parsed_body['password']
+    
+    except Exception as e:
+        return response(400, { "error": str(e) })
+    
+    #TODO: Check user is registered and password is correct
+
+    # Find user by email
+    query = table.query(
+        IndexName="EmailIndex",
+        KeyConditionExpression=Key("email").eq(email)
+    )
+
+    items = query.get("Items", [])
+    user = items[0] if items else None
+
+    if not user:
+        raise Exception("Bad credentials")
+    
+    # Check password
+
+    passwordBytes = password.encode('utf-8')
+    passwordHash = user.get("passwordHash").encode('utf-8')
+    isPasswordValid = bcrypt.checkpw(passwordBytes, passwordHash)
+
+    if not isPasswordValid:
+        raise Exception("Bad credentials")
+
+    #TODO: extract roles of user
+    roles = []
+
+    token = jwt.encode({'id': user.get('id'), 'roles': roles }, JWT_SECRET, algorithm='HS256')
+
+    body = json.dumps({ 
+        "token": token
+    })
 
     return {
         "statusCode": 200,
--- url-shortener-backend/src/auth/register/app.py
+++ url-shortener-backend/src/auth/register/app.py
@@ -1,9 +1,68 @@
 import json
+from json import JSONDecodeError
+
 import boto3
 import os
+import jwt
+
+import bcrypt
+
+from uuid import uuid4
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(os.environ["USER_TABLE_NAME"])
+
+JWT_SECRET = os.environ["JWT_SECRET"]
+
+def response(status, body_dict):
+    return {
+        "statusCode": status,
+        "headers": {
+            "Content-Type": "application/json",
+        },
+        "body": json.dumps(body_dict)
+    }
+
 
 def handler(event, _context):
+
+    request_body = event.get('body')
+
+    try:
+        parsed_body = json.loads(request_body)
+        email = parsed_body['email']
+        password = parsed_body['password']
+    
+    except Exception as e:
+        return response(400, { "error": str(e) })
+
+    #TODO: Check password satisfies requirements (min length, special symbols...)
+
+    #TODO: Check if user is already registered
+    # Write new user entry
+
+    ## Hash password
+    salt = bcrypt.gensalt()
+
+    passwordBytes = password.encode('utf-8')
+    passwordHash = bcrypt.hashpw(passwordBytes, salt)
+
+    userId = str(uuid4())
+
+    user = {
+        "id": userId,
+        "email": email,
+        "passwordHash": passwordHash.decode('utf-8')
+    }
+
+    ## Write in dynamoDB
+    table.put_item(Item=user)
+
+    token = jwt.encode({"User": {"id": userId}}, JWT_SECRET, algorithm='HS256')
+
     return {
         "statusCode": 200,
-        "body": "OK"
+        "body": json.dumps({
+            "token": token
+        })
     }
